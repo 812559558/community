c3bd38624f62f20fee694fa5cd7afb83d0678e04
huhaonan <812559558@qq.com>
2023-05-10 16:55:23 +0800

    完成：查看帖子详情功能

--- src/main/java/com/nowcoder/community/service/DiscussPostService.java
+++ src/main/java/com/nowcoder/community/service/DiscussPostService.java
@@ -31,4 +31,7 @@ public class DiscussPostService {
         post.setContent(sensitiveFilter.filter(post.getContent()));
         return discussPostMapper.insertDiscussPost(post);
     }
+    public DiscussPost findDiscussPostById(int id){
+        return discussPostMapper.selectDiscussPostById(id);
+    }
 }
