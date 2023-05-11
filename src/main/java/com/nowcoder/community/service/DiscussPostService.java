1ec719c42617bac8d74c1c91991bbba34c8b6dd3
huhaonan <812559558@qq.com>
2023-05-11 22:01:52 +0800

    完成：增加评论

--- src/main/java/com/nowcoder/community/service/DiscussPostService.java
+++ src/main/java/com/nowcoder/community/service/DiscussPostService.java
@@ -15,14 +15,17 @@ public class DiscussPostService {
     private DiscussPostMapper discussPostMapper;
     @Autowired
     private SensitiveFilter sensitiveFilter;
-    public List<DiscussPost> fineDiscussPosts(int userId,int offset,int limit){
-        return discussPostMapper.selectDiscussPosts(userId,offset,limit);
+
+    public List<DiscussPost> fineDiscussPosts(int userId, int offset, int limit) {
+        return discussPostMapper.selectDiscussPosts(userId, offset, limit);
     }
-    public int findDiscussPostRows(int userId){
+
+    public int findDiscussPostRows(int userId) {
         return discussPostMapper.selectDiscussPostRows(userId);
     }
-    public int addDiscussPost(DiscussPost post){
-        if(post == null){
+
+    public int addDiscussPost(DiscussPost post) {
+        if (post == null) {
             throw new IllegalArgumentException("参数不能为空！");
         }
         post.setTitle(HtmlUtils.htmlEscape(post.getTitle()));
@@ -31,7 +34,12 @@ public class DiscussPostService {
         post.setContent(sensitiveFilter.filter(post.getContent()));
         return discussPostMapper.insertDiscussPost(post);
     }
-    public DiscussPost findDiscussPostById(int id){
+
+    public DiscussPost findDiscussPostById(int id) {
         return discussPostMapper.selectDiscussPostById(id);
+    }
+
+    public int updateCommentCount(int id, int commentCount) {
+        return discussPostMapper.updateCommentCount(id, commentCount);
     }
 }
