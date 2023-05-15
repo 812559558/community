6492bb7659f9ebd328d7cba112208ab9fdfcb60e
huhaonan <812559558@qq.com>
2023-05-15 09:06:53 +0800

    完成：统一处理异常

--- src/main/java/com/nowcoder/community/controller/HomeController.java
+++ src/main/java/com/nowcoder/community/controller/HomeController.java
@@ -19,23 +19,28 @@ public class HomeController {
     private DiscussPostService discussPostService;
     @Autowired
     private UserService userService;
-    @RequestMapping(path = "/index",method = RequestMethod.GET)
-    public String getIndexPage(Model model, Page page){
+
+    @RequestMapping(path = "/index", method = RequestMethod.GET)
+    public String getIndexPage(Model model, Page page) {
         page.setRows(discussPostService.findDiscussPostRows(0));
         page.setPath("/index");
         List<DiscussPost> list = discussPostService.fineDiscussPosts(0, page.getOffset(), page.getLimit());
         List<Map<String, Object>> discussPosts = new ArrayList<>();
-        if(list!=null){
-            for(DiscussPost post:list){
+        if (list != null) {
+            for (DiscussPost post : list) {
                 HashMap<String, Object> map = new HashMap<>();
-                map.put("post",post);
+                map.put("post", post);
                 User user = userService.findUserById(post.getUserId());
-                map.put("user",user);
+                map.put("user", user);
                 discussPosts.add(map);
             }
         }
-        model.addAttribute("discussPosts",discussPosts);
+        model.addAttribute("discussPosts", discussPosts);
         return "index";
     }
 
+    @RequestMapping(path = "/error", method = RequestMethod.GET)
+    public String getErrorPage() {
+        return "/error/500";
+    }
 }
