8ec2df4279ca5ebc42efc2b7086b1b51df8be46d
huhaonan <812559558@qq.com>
2023-05-24 20:09:58 +0800

    完成：整合Elasticsearch，完成基本功能测试 胡浩楠0524

--- src/main/java/com/nowcoder/community/entity/DiscussPost.java
+++ src/main/java/com/nowcoder/community/entity/DiscussPost.java
@@ -1,16 +1,33 @@
 package com.nowcoder.community.entity;
 
-import java.util.Date;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
 
+import java.util.Date;
+@Document(indexName = "discusspost",type = "_doc",shards = 6,replicas =3)
 public class DiscussPost {
+    //索引， type被弱化了以后会取消，分片，副本
+    @Id
     private int id;
+
+    @Field(type = FieldType.Integer)
     private int userId;
+    @Field(type = FieldType.Text , analyzer = "ik_max_word",searchAnalyzer = "ik_smart")
     private String title;
+    //类型，存储时的解析器，搜索时的解析器
+    @Field(type = FieldType.Text , analyzer = "ik_max_word",searchAnalyzer = "ik_smart")
     private String content;
+    @Field(type = FieldType.Integer)
     private int type;
+    @Field(type = FieldType.Integer)
     private int status;
+    @Field(type = FieldType.Date)
     private Date createTime;
+    @Field(type = FieldType.Integer)
     private int commentCount;
+    @Field(type = FieldType.Double)
     private double score;
 
     @Override
